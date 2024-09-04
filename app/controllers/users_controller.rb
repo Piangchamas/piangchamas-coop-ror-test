6e7902e84a21f1af9bfa670415f9cc600a13f9c1
piangchamas <piangchamas@odds.team>
2024-09-04 21:38:22 +0700

    update a userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to root_path, notice: "Registration successful"
+    else
+      render :new
+    end
+  end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
   end
 end
