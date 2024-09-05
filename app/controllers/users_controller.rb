20ac28d6793ffd54163ea1abfaf038bf314dcb30
piangchamas <piangchamas@odds.team>
2024-09-05 13:09:02 +0700

    added dashboard displaying data registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save 
-      redirect_to root_path, notice: "Registration successful"
+      redirect_to '/dashboard', notice: "Registration successful"
     else
       render :new
     end
   end
   def user_params
     params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+  end
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'register', to: 'users#new'
   post 'register', to: 'users#create'
   root 'users#new'
+  get 'dashboard', to: 'users#dashboard'
 end
